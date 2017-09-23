466901453cfcf064100a96a984ece65da4310744
James Hughes <james.a.hughes@gmail.com>
2017-09-23 13:34:39 -0500

    Add index, show, new and create actions

--- app/controllers/junks_controller.rb
+++ app/controllers/junks_controller.rb
@@ -1,3 +1,21 @@
 class JunksController < ApplicationController
-  
+  def index
+    @junks = Junk.all
+    render 'index.html.erb'
+  end
+
+  def new
+    render 'new.html.erb'
+  end
+
+  def create
+    junk = Junk.create(name: params["name"])
+    render 'create.html.erb'
+  end
+
+  def show
+    junk_id =  params[:id]
+    @junk = Junk.find_by(id: junk_id)
+    render 'show.html.erb'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get "/junks" => "junks#index"
-  get "/junks/new" => "junks#new"
-  post "/junks" => "junks#create"
-  get "/junks/:id" => "junks#show"
-  get "/junks/:id/edit" => "junks#edit"
-  patch "/junks/:id" => "junks#update"
-  delete "/junks/:id" => "junks#destroy"
+  get '/junks' => 'junks#index'
+  get '/junks/new' => 'junks#new'
+  post '/junks' => 'junks#create'
+  get '/junks/:id' => 'junks#show'
+  get '/junks/:id/edit' => 'junks#edit'
+  patch '/junks/:id' => 'junks#update'
+  delete '/junks/:id' => 'junks#destroy'
 end
